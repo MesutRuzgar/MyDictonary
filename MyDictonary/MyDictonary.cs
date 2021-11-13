9a6a4645cc1286abdabae0883a73e33159f14692
MesutRuzgar <mesutruzgar@gmail.com>
2021-11-13 15:57:03 +0300

    Add project files.

---
+++ MyDictonary/MyDictonary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+
+namespace MyDictonary
+{
+    class MyDictonary<Tid, Tname>
+    {
+        Tid[] ids;
+        Tname[] names;
+
+        public MyDictonary()
+        {
+            ids = new Tid[0];
+            names = new Tname[0];
+        }
+               
+        public void Add(Tid id, Tname name)
+        {
+            Tid[] tempId = ids;
+            ids = new Tid[ids.Length + 1];
+            Tname[] tempName = names;
+            names = new Tname[names.Length + 1];
+
+            for (int i = 0; i < tempId.Length; i++)
+            {
+                ids[i] = tempId[i];
+                names[i] = tempName[i];
+            }
+            ids[ids.Length - 1] = id;
+            names[names.Length - 1] = name;
+                      
+            Console.WriteLine("Id : " + id + "        Name : " + name);
+        }
+
+    }
+
+}
+
+
+
---
+++ MyDictonary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictonary<int, string> Ogrenci = new MyDictonary<int, string>();
+            Ogrenci.Add(1, "Okan");
+            Ogrenci.Add(2, "Mesut");
+            Ogrenci.Add(3, "Furkan");
+            Ogrenci.Add(4, "Umut");
+            Ogrenci.Add(5, "Mehmet");
+       
+        }
+
+
+    }
+}
+
